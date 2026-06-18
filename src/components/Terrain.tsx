9d58a59e02f330303a5e158915d20d22026cc591
DoctorTone <tony@drt-software.com>
2026-06-18 23:01:35 +0100

    Increase terrain size

--- src/components/Terrain.tsx
+++ src/components/Terrain.tsx
@@ -1,7 +1,7 @@
-const Terrain = ({ lat, lon }) => {
+const Terrain = () => {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-      <planeGeometry args={[2000, 2000]} />
+      <planeGeometry args={[5000, 5000]} />
       <meshStandardMaterial color="#1a3a1a" roughness={0.8} metalness={0.1} />
     </mesh>
   );
